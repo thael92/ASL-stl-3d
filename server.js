1f9b0324fa7d5b43eba2d957713a2b6fa59aed6d
thael92 <thael92@gmail.com>
2025-06-20 13:46:43 -0300

    ajuste js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/list-files', (req, res) => {
 });
 
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 );
 
 app.listen(PORT, () =>
